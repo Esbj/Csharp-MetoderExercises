f51385016abb63e02813835a2f6d1974e996a090
Alexander <alex.esbj@gmail.com>
2019-09-23 18:41:43 +0200

    Gjorde klart Add Strings

--- Add-Strings/Program.cs
+++ Add-Strings/Program.cs
@@ -4,9 +4,18 @@ namespace Add_Strings
 {
     class Program
     {
+        static string addStrings (string string1, string string2){
+            string addedString = string1 +" "+string2;
+            return addedString;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Skapa en metod som tar in två string variabler, lägger samman dessa och
+            // returnerar som en sträng. (Tex. Strängarna ”Hej på dig!” och ” Hur mår
+            // du?” blir ”Hej på dig! Hur mår du?”.
+            Console.WriteLine("Skriv in två namn: ");
+            string namn = addStrings(Console.ReadLine(), Console.ReadLine());
+            Console.WriteLine(namn);
         }
     }
 }
