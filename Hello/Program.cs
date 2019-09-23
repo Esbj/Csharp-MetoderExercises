9c05ed1bd6ab1c8360cc990239c4394b691eea0b
Alexander <alex.esbj@gmail.com>
2019-09-23 18:24:31 +0200

    Gjorde klart Hello

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -4,9 +4,20 @@ namespace Hello
 {
     class Program
     {
+        static void Hello(string namn){
+            Console.WriteLine("Hello "+namn); 
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Skapa en metod Hello() som tar en string variabel som indata och skriver
+            // ut texten med ett annat meddelande
+
+            //Metod syntax:
+            // static <void(odefinerat), int, bool, etc..> <namn> (utdata)  {
+            //     kod kod kod
+            //     ev return 
+            // }
+            Hello("Göte");
         }
     }
 }
