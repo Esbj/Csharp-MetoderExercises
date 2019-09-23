e402de3c15a6282987e8c664f56ab0d138d9e1ee
Alexander <alex.esbj@gmail.com>
2019-09-23 23:30:07 +0200

    Gjorde klart addera
    Tycker den är rättså dry och nöjd med resultatet (:

--- Add-Numbers/Program.cs
+++ Add-Numbers/Program.cs
@@ -5,13 +5,17 @@ namespace Add_Numbers
     //Skapa metoden Addera() som tar in två heltal och returnerar summan.
     class Program
     {
-        static int Addera (int tal1, int tal2){
+        static int Addera (){
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            int tal2 = Convert.ToInt32(Console.ReadLine());
             int summa = tal1 + tal2;
             return summa;
         }
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Mata in 2 heltal: ");
+            int summa = Addera();
+            Console.WriteLine("Summan är "+summa);
         }
     }
 }
