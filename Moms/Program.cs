4faa25dc263c5c3da931f73cb455d224c59d7253
Alexander <alex.esbj@gmail.com>
2019-09-23 23:48:15 +0200

    Moms del 1 funkar...
    IDK how it works, but it works

--- Moms/Program.cs
+++ Moms/Program.cs
@@ -4,9 +4,25 @@ namespace Moms
 {
     class Program
     {
+        static double Moms (double noMoms){
+            double momsValue = noMoms / 4;
+            double plusMoms = momsValue + noMoms;
+            return plusMoms;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Skapa en metod som lägger till momsen på en viss summa. Antag att
+            //momsen är 25 %.
+
+            Console.WriteLine("Ange priset du vill lägga till momms på:");
+
+
+            
+            int intCost = Convert.ToInt32(Console.ReadLine());
+            double doubleCost = Convert.ToDouble(intCost);
+            Console.WriteLine(Moms(doubleCost));
+
+
         }
     }
 }
