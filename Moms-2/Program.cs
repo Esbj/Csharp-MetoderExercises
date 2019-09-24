5b46d73e524118a289bdfd9ae883fcfd7d8e0521
Alexander <alex.esbj@gmail.com>
2019-09-24 13:47:53 +0200

    Klar med del 2 av moms
    Kan behövas mer beskrivande kommentarer...

--- Moms-2/Program.cs
+++ Moms-2/Program.cs
@@ -4,14 +4,13 @@ namespace Moms
 {
     class Program
     {
-        //Måste använda double på indatan för att kunna räkna ut momsen då tilldelningsoperatorn tar med 
-        //datavärde på referens
-        static double Moms (double noMoms){
-            //Räknar ut moms och sparar i separat värde
-            double momsValue = noMoms / 4;
-            //Returnerar pris + moms...
-            double plusMoms = momsValue + noMoms;
-            return plusMoms;
+        //Funktionen tar in 2 värden: Pris och Moms och returnerar pris + moms.
+        static double Moms(double price, double moms)
+        {
+            double deciMoms = moms/100;
+            double fullPrice = deciMoms+price;
+            return fullPrice;
+
         }
         static void Main(string[] args)
         {
@@ -19,12 +18,21 @@ namespace Moms
             //momsen är 25 %.
 
 
+            // Gör om funktionen så att man i
+            // metodanropet(via parameterlistan) får ange vad momsen ligger på.
+
+
 
             //Tar in pris från användaren och gör om till värde som funktionen behöver
-            Console.WriteLine("Ange priset du vill lägga till momms på:");
-            double input = Convert.ToDouble(Console.ReadLine());
-            //Kalla på funktionen
-            Console.WriteLine(Moms(input));
+            Console.Write("Ange priset du vill lägga till momms på: ");
+            double priceInput = Convert.ToDouble(Console.ReadLine());
+            //Tar in värde på moms
+            Console.Write("Ange nuvarande moms: ");
+            double momsInput = Convert.ToDouble(Console.ReadLine());
+
+            //Kalla på funktionen och skriv ut
+            double pris = Moms(priceInput, momsInput);
+            Console.WriteLine("Pris inklusive moms = "+pris);
 
 
 
