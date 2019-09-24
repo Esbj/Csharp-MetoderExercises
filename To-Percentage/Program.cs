77221699e9e071511db19f9e779624640109719d
Alexander <alex.esbj@gmail.com>
2019-09-24 14:07:34 +0200

    Gjorde klart To-Precentage
    Self documenting code ;)

--- To-Percentage/Program.cs
+++ To-Percentage/Program.cs
@@ -4,8 +4,13 @@ namespace To_Percentage
 {
     class Program
     {
+        static int ToPercentage(float input){
+            int precentageValue = Convert.ToInt32(input)*100;
+            return precentageValue;
+        }
         static void Main(string[] args)
         {
+            // Skapa metoden ToPercentage() som tar in ett decimaltal och returnerar ett heltal.
             Console.WriteLine("Hello World!");
         }
     }
