bea70e3827a10ad0556cbe662c60896bf495de38
Alexander <alex.esbj@gmail.com>
2019-09-24 15:52:25 +0200

    Gjorde klart myndig
    if else...

--- Legal/Program.cs
+++ Legal/Program.cs
@@ -4,13 +4,27 @@ namespace legal
 {
     class Program
     {
+        static bool legal (int age){
+            if (age < 18){
+                return false;
+            }else{
+                return true;
+            }
+        }
         static void Main(string[] args)
         {
             //Skapa en metod som kontrollerar om man är myndig.
             //Metoden ska ta in ett heltal och returnera true eller false.
             
             
-            Console.WriteLine("Hello World!");
+           Console.WriteLine("Ange din ålder: ");
+           int ålder = Convert.ToInt32(Console.ReadLine());
+           bool myndig = legal(ålder);
+           if (myndig){
+            Console.WriteLine("Du är myndig!");
+           }else{
+            Console.WriteLine("Du är inte myndig!");
+           }
         }
     }
 }
